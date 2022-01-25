711c7d286bd7a86ea1e92feee9b34e120ce38d5e
ricardoddm <ricky.ddm@hotmail.com>
2022-01-25 14:54:13 -0600

    cambios logo

--- js/packages/web/src/components/AppBar/index.tsx
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -51,7 +51,7 @@ export const MetaplexMenu = () => {
     return (
       <>
         <Modal
-          title={<img src={'/metaplex-logo.svg'} />}
+          title={<img src={'/investel-logo.png'} />}
           visible={isModalVisible}
           footer={null}
           className={'modal-box'}
@@ -110,7 +110,7 @@ export const MetaplexMenu = () => {
 export const LogoLink = () => {
   return (
     <Link to={`/`}>
-      <img src={'/metaplex-logo.svg'} />
+      <img className="logo" src={'/investel-logo.png'} />
     </Link>
   );
 };
